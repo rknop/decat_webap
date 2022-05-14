fd599c172691175e5123000059bb0963b9090f64
Rob Knop (Nersc) <raknop@lbl.gov>
2022-05-14 02:38:49 -0700

    production config files

---
+++ decatview_config_production.js
@@ -0,0 +1,19 @@
+var webapconfig = {
+    "webapfullurl": "http://decatdb.lbl.gov/decatview.py/",
+    "webapurl": "/decatview.py/",
+    "defaultrbtype": 2,
+    "proposals": {
+        "2021A-0113": "2021A-0113: Graham DDF Spring 2021",
+        "2021B-0149": "2021B-0149: Graham DDF Fall 2021",
+        "2022A-724693": "2022A-724693: Graham DDF Spring 2022",
+        "2022A-388025": "2022A-388025: DESI-RT Spring 2022",
+        "2021A-0275": "2021A-0275: Rest YSE",
+        "2020B-0053": "2020B-0053: Brount DEBASS "
+    },
+    "defaultproposals": [ "2022A-724693" ],
+
+    "vetdefaultgalexgal": "Galactic",       // "Galactic" or "Extragalactic"
+    "vetdefaultshowothers": "No",           // "Yes" or "No"
+}
+    
+export { webapconfig }
